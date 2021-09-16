dfab8e04a3c46504c5a81c515004c68e9b43ca50
yohann <yohann.valo@utt.fr>
2021-09-16 10:53:53 +0200

    correctif sur la timezone

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from ics import Calendar, Event
+from pytz import timezone
 import os
 
+tz = timezone("Europe/Paris")
 c = Calendar() # creation d'un calendrier
-xl = pd.read_excel("votreEDT.xlsx", usecols="B:H,N,O") # on garde les colonnes de date matiere et salles de classe
+xl = pd.read_excel("EduTemps21.xlsx", usecols="B:H,N,O") # on garde les colonnes de date matiere et salles de classe
 for i in range(len(xl)):
     for j in range(1, 7):
         if isinstance(xl.iloc[i][j], str) and len(xl.iloc[i][j]) > 1: # on parcourt le dataframe en excluant les valeurs nulles ou les espaces simples
@@ -13,30 +15,30 @@ for i in range(len(xl)):
 
             if j < 3:
                 e.location = str(xl.iloc[i][7]) #ici le salles sont repartis en fonction du matin et de l apres midi donc
-            else:                               #avant midi on se situera sur la colonne 7 et apres midi sur la 8
+            elif j > 3:                              #avant midi on se situera sur la colonne 7 et apres midi sur la 8
                 e.location = str(xl.iloc[i][8])
 
             dateDebut = xl.iloc[i][0] # ce sont les dates du jours au format annee-mois-jour 00h00 on va donc changer l'heure
             dateFin = xl.iloc[i][0]
 
-            if j == 1:# grouoe de condition un peu degueu qui vont changer l heure de fin de la date pour s'accorder avec l excel
-                dateDebut = dateDebut.replace(hour=8)
-                dateFin = dateFin.replace(hour=9)
+            if j == 1:# groupe de condition un peu degueu qui vont changer l'heure de fin de la date pour s'accorder avec l excel
+                dateDebut = dateDebut.replace(hour=8, tzinfo=tz)
+                dateFin = dateFin.replace(hour=9, tzinfo=tz)
             elif j == 2:
-                dateDebut = dateDebut.replace(hour=9)
-                dateFin = dateFin.replace(hour=10)
+                dateDebut = dateDebut.replace(hour=9, tzinfo=tz)
+                dateFin = dateFin.replace(hour=10, tzinfo=tz)
             elif j == 3:
-                dateDebut = dateDebut.replace(hour=10)
-                dateFin = dateFin.replace(hour=12)
+                dateDebut = dateDebut.replace(hour=10, tzinfo=tz)
+                dateFin = dateFin.replace(hour=12, tzinfo=tz)
             elif j == 4:
-                dateDebut = dateDebut.replace(hour=12)
-                dateFin = dateFin.replace(hour=14)
+                dateDebut = dateDebut.replace(hour=12, tzinfo=tz)
+                dateFin = dateFin.replace(hour=14, tzinfo=tz)
             elif j == 5:
-                dateDebut = dateDebut.replace(hour=14)
-                dateFin = dateFin.replace(hour=16)
+                dateDebut = dateDebut.replace(hour=14, tzinfo=tz)
+                dateFin = dateFin.replace(hour=16, tzinfo=tz)
             elif j == 6:
-                dateDebut = dateDebut.replace(hour=16)
-                dateFin = dateFin.replace(hour=18)
+                dateDebut = dateDebut.replace(hour=16, tzinfo=tz)
+                dateFin = dateFin.replace(hour=18, tzinfo=tz)
 
             e.begin = dateDebut # on affecte les dates a l 'event
             e.end = dateFin
@@ -44,6 +46,3 @@ for i in range(len(xl)):
 
 with open('my.ics', 'w') as f:
     f.write(str(c)) #on creer un fichier ics ou on écrit le calendrier en format ics
-
-cmd = 'sed -i "s/0000Z/0000/g" my.ics' # petite commande qui permet de régler un problème de timezone qui se met par defaut en UTC on supprime ici le Z
-os.system(cmd)
